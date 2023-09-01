fc98249faec9633678a254e1cb8a78905dace4e1
Ferney Steven Diaz Puentes <ferney.diaz@AvalDigitalLabs.com>
2023-09-01 12:34:39 -0500

    First commit

--- src/test/java/com/avaldigitallabs/facilpass/navigation/LoginPage.java
+++
@@ -1,8 +0,0 @@
-package com.avaldigitallabs.facilpass.navigation;
-
-import net.serenitybdd.core.pages.PageObject;
-import net.thucydides.core.annotations.DefaultUrl;
-
-@DefaultUrl("https://fap-commons-stg.avaldigitallabs.com/#/signin/login")
-public class LoginPage extends PageObject{
-}
